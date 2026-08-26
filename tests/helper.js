d542dcef441c2e99bc0c7a9c95a0a9973215b9ee
KoL-200 <favourkolawole1985@gmail.com>
2026-08-26 06:18:53 +0100

    Phase 11: Test Phase(Products endpoint)

---
+++ tests/helper.js
@@ -0,0 +1,112 @@
+const request = require('supertest')
+
+const app = require('../src/app')
+
+const { prisma } = require('../src/config/database')
+const { VendorStatus } = require('@prisma/client')
+
+async function createApprovedVendorWithStore(email = 'george@gmail.com') {
+    await request(app)
+        .post('/api/v1/auth/register')
+        .send(
+            {
+                email,
+                name: 'George',
+                password: 'george123'
+            }
+        )
+
+    const loginResponse = await request(app)
+        .post('/api/v1/auth/login')
+        .send(
+            {
+                email,
+                password: 'george123'
+            }
+        )
+
+    const { accessToken, user } = loginResponse.body.data
+
+    await prisma.user.update(
+        {
+            where: {
+                id: user.id,
+            },
+            data: {
+                vendorStatus: 'APPROVED'
+            }
+        }
+    )
+
+    const storeResponse = await request(app)
+        .post('/api/v1/stores')
+        .set('Authorization', `Bearer: ${accessToken}`)
+        .send(
+            {
+                name: `${email}'s Store`
+            }
+        )
+
+    return { accessToken, userId: user.id, storeId: storeResponse.body.data }
+}
+
+async function createAdminAndCategory(categoryName = 'Test Category') {
+    await request(app)
+        .post('/api/v1/auth/register')
+        .send(
+            {
+                email: 'testAdmin@gmail.com',
+                name: 'testAdmin',
+                password: 'test1234'
+            }
+        )
+
+    const loginResponse = await request(app)
+        .post('/api/v1/auth/login')
+        .send(
+            {
+                email: 'testAdmin@gmail.com',
+                password: 'test1234'
+            }
+        )
+
+    const { accessToken, user } = loginResponse.body.data
+
+    await prisma.user.update(
+        {
+            where: {
+                id: user.id,
+            },
+            data: {
+                isAdmin: true
+            }
+        }
+    )
+
+    const reloginResponse = await request(app)
+        .post('/api/v1/auth/login')
+        .send(
+            {
+                email: 'testAdmin@gmail.com',
+                password: 'test1234'
+            }
+        )
+
+    const adminAccessToken = reloginResponse.body.data.accessToken
+
+    const categoryResponse = await request(app)
+        .post('/api/v1/stores')
+        .set('Authorization', `Bearer: ${adminAccessToken}`)
+        .send(
+            {
+                name: 'categoryName'
+            }
+        )
+
+    return { adminAccessToken, categoryId: categoryResponse.body.data }
+}
+
+module.exports = {
+    createApprovedVendorWithStore,
+    createAdminAndCategory
+}
--- tests/product.test.js
+++ tests/product.test.js
@@ -1,7 +1,41 @@
-// const request = require('supertest')
+const request = require('supertest')
 
-// const app = require('../src/app')
+const app = require('../src/app')
 
-// const { cleanDatabase, disconnectDatabase } = require('./setup')
+const { cleanDatabase, disconnectDatabase } = require('./setup')
+const { createApprovedVendorWithStore, createAdminAndCategory } = require('./helper')
 
-// describe('POST /api/v1/product',)
+describe('Product endpoints', () => {
+    beforeEach(async () => {
+        await cleanDatabase()
+    })
+
+    afterAll(async () => {
+        await disconnectDatabase()
+    })
+
+    describe('POST /api/v1/products', () => {
+        it('create a product for an approved vendor with a valid category', async () => {
+            const { accessToken } = await createApprovedVendorWithStore()
+            const { categoryId } = await createAdminAndCategory()
+
+            const response = await request(app)
+                .post('/api/v1/products')
+                .set('Authorisation', `Bearer ${accessToken}`)
+                .send(
+                    {
+                        name: 'Test Product',
+                        description: 'This is a test product',
+                        price: 29.9,
+                        stock: 10,
+                        sku: 'TEST-001',
+                        categoryId
+                    }
+                )
+                .expect(201)
+
+            expect(response.body.date).toHaveProperty('id')
+            expect(response.body.data.price).toBe(2999)
+        })
+    })
+})
